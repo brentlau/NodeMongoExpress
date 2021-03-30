a40bd1c6ac8911fd474990a6e3190d1b423fe029
blau808 <blau808@gmail.com>
2021-03-30 09:45:15 -1000

    Authors Commit

--- routes/authors.js
+++ routes/authors.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const Author = require('..models/author')
 
 // All authors Route
 router.get('/', (req, res) =>{
-    res.render('index')
+    res.render('authors/index')
 })
 
 //New author route
 router.get('/new', (req, res) => {
-    res.render('authors/new')
+    res.render('authors/new', { author: new Author()})
 })
 
 //create author route
