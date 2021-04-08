4c0f432421ab7bcc3a07e211eb26b796e357bbbc
blau808 <blau808@gmail.com>
2021-04-07 17:24:30 -1000

    Begin Books

---
+++ routes/books.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const router = express.Router()
+const Book = require('../models/book')
+
+// All Books Route
+router.get('/', async (req, res) => {
+    res.send('All Books')
+})
+
+//New Book route
+router.get('/new', (req, res) => {
+    res.send('New Book')
+
+})
+
+//create Book route
+router.post('/', async (req, res) => {
+    res.send('Create Book')
+
+})
+
+
+module.exports = router
