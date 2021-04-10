9a207e7894d55c0f53d9bcd50f82b19dcd5f11ee
blau808 <blau808@gmail.com>
2021-04-09 16:11:55 -1000

    Commit for home

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const multer = require('multer')
+const path = require('path')
 const Book = require('../models/book')
+const uploadPath = path.join('public', Book. coverImageBasePath)
+const imageMimeTypes = ['image/jpeg', 'image/png', 'image/gif']
+const Author = require('../models/author')
+const upload = multer({
+    dest: uploadPath,
+    fileFilter: (req, file, callback) => {
+        callback(null, )
+    }
+})
 
 // All Books Route
 router.get('/', async (req, res) => {
@@ -8,14 +19,30 @@ router.get('/', async (req, res) => {
 })
 
 //New Book route
-router.get('/new', (req, res) => {
-    res.send('New Book')
+router.get('/new', async (req, res) => {
+    try{
+        const authors = await Author.find({})
+        const book = new Book()
+        res.render('books/new',  {
+            authors: authors,
+            book: book
+        })
+    } catch {
+        res.redirect('/books')
+
+    }
 
 })
 
 //create Book route
 router.post('/', async (req, res) => {
-    res.send('Create Book')
+    const book = new Book({
+        title: req.body.title,
+        author: req.body.author,
+        publishDate: new Date(req.body.publishDate),
+        pageCount: req.body.pageCount,
+        description: req.body.description
+    })
 
 })
 
